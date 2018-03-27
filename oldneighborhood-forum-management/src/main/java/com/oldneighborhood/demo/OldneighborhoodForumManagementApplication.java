0cc332d3325489a2c34441bb3ebd708336b4b348
RenBrian <Brian_Ren@outlook.com>
2018-03-27 17:10:55 +0800

    "init"

---
+++ oldneighborhood-forum-management/src/main/java/com/oldneighborhood/demo/OldneighborhoodForumManagementApplication.java
@@ -0,0 +1,12 @@
+package com.oldneighborhood.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldneighborhoodForumManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldneighborhoodForumManagementApplication.class, args);
+	}
+}
